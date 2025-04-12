6fa05a39934a960a5a747441c1f992fdf288d9ae
Stefan Penner <stef@iamstef.net>
2025-04-12 18:47:21 +0000

    more experiments

--- tamper.user.js
+++ tamper.user.js
@@ -1,15 +1,18 @@
 // ==UserScript==
-// @name         Stefs Hack
+// @name         tamper with lcc.live for the learnings
 // @namespace    stefanpenner
 // @version      2025-04-12
-// @description  try to take over the world!
-// @author       You
+// @description  just refreshing my memory
+// @author       stefanpenner
 // @match        https://lcc.live/*
 // @icon         https://lcc.live
-// @updateURL    https://raw.githubusercontent.com/stefanpenner/tamper/refs/heads/main/tamper.user.js
-// @downloadURL  https://raw.githubusercontent.com/stefanpenner/tamper/refs/heads/main/tamper.user.js
+// @updateURL    https://raw.githubusercontent.com/stefanpenner/-lcc-tamper-experiment/refs/heads/main/tamper.user.js
+// @downloadURL  https://raw.githubusercontent.com/stefanpenner/-lcc-tamper-experiment/refs/heads/main/tamper.user.js
 // @run-at       document-start
-// @grant        none
+// @grant GM_registerMenuCommand
+// @license      MIT
+// @supportURL   https://github.com/stefanpenner/-lcc-tamper-experiment/issues
+// @homepageURL  https://github.com/stefanpenner/-lcc-tamper-experiment
 // ==/UserScript==
 
 (function() {
@@ -18,6 +21,11 @@
     h1.textContent = "You've been tampered with"
     document.body.prepend(h1)
     debugger
+    function test() {
+        alert('Hi');
+      }
+
+      GM_registerMenuCommand("Alert Hi", test, "e");
     // Prevent initial script execution
     for (let script of document.querySelectorAll('script')) {
         if (script.src && script.src.includes('/s/script.js')) {
